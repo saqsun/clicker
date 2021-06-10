bd2e768b39b7842ecf23eb545ec391ab104acb2a
saqsun <sargsyan.sargis.89@gmail.com>
2021-06-10 15:13:07 +0400

    new level config

--- src/constants/configs/level-configs.ts
+++ src/constants/configs/level-configs.ts
@@ -1,43 +1,43 @@
 export let levelConfigs: LevelConfig[] = [
     {
         bots: { count: 8, startHp: 20, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 150 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 80, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 60, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 120, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 180, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 240, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 300, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 360, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 420, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 480, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
-        bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
-        boss: { incrementHp: { min: 50, max: 100 }, money: 1 },
+        bots: { count: 8, startHp: 540, incrementHp: { min: 5, max: 10 }, money: 1 },
+        boss: { incrementHp: { min: 50, max: 100 }, money: 5 },
     },
     {
         bots: { count: 8, startHp: 800, incrementHp: { min: 5, max: 10 }, money: 1 },
